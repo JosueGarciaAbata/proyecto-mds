9a0c1f0c3e963019df02d9cd55c986bf3b29b92a
carol104 <109120696+carol104@users.noreply.github.com>
2024-05-20 14:04:59 -0500

    Update header_menu.php

--- usuarioNuevo/header_menu.php
+++ usuarioNuevo/header_menu.php
@@ -42,7 +42,7 @@
             </a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="discover">
+            <a class="nav-link" href="discover.php">
               <span
                 class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
